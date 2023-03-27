4d7b9e116dba0ed49706f9e4b2e44d07eafc64fc
SolusBlade <kykysha59fafa@gmail.com>
2023-03-27 19:55:07 +0300

    done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,49 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import Feedback from './Feedback/Feedback';
+import Statistics from './Statistics/Statistics';
+import Section from './Section/Section';
+
+class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  onAddFeedback = e => {
+    const { id } = e.target;
+    this.setState(prev => ({
+      [id]: prev[id] + 1,
+    }));
+  };
+  countTotalFeedback = () => {
+    const { good, neutral, bad } = this.state;
+    return good + neutral + bad;
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    const { good } = this.state;
+    const result = Math.round((good * 100) / this.countTotalFeedback());
+    if (!result) return 0;
+    return result;
+  };
+
+  render() {
+    return (
+      <>
+        <Section title={'Please leave feedback'}>
+          <Feedback onAddFeedback={this.onAddFeedback} />
+        </Section>
+        <Section title={'Statistics'}>
+          <Statistics
+            stat={this.state}
+            total={this.countTotalFeedback()}
+            positiveFeedbackPercentage={this.countPositiveFeedbackPercentage()}
+          />
+        </Section>
+      </>
+    );
+  }
+}
+
+export default App;
